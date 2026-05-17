1a72b8548fade4d7cff119e8da7fe938d872a0fc
yaxyobekuz <pubgn9642@gmail.com>
2026-05-17 11:48:49 +0500

    feat: remove block and unblock handlers for drivers

--- src/modules/drivers/handlers/unblock.handler.js
+++
@@ -1,9 +0,0 @@
-import asyncHandler from "../../../middleware/asyncHandler.js";
-import * as driversService from "../services/drivers.service.js";
-
-const unblock = asyncHandler(async (req, res) => {
-  const data = await driversService.unblock(req.params.id);
-  res.json({ success: true, data, message: "Haydovchi blokdan chiqarildi" });
-});
-
-export default unblock;
